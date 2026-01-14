957bbaa2028c79b7ee5e980858ea933b4dceadaf
wsq1231321q <netflix.ken3.2026@gmail.com>
2026-01-14 15:25:57 -0500

    Create CorsConfig.java

---
+++ src/main/java/com/lavanderia/userservice/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.lavanderia.userservice.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")   // permite todos los orígenes
+                .allowedMethods("*")          // GET, POST, PUT, DELETE, etc
+                .allowedHeaders("*")
+                .allowCredentials(false);     // debe ser false si usas "*"
+    }
+}
